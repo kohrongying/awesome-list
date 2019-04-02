2ede942653331d7dfd4e79ad98fb0d7312136cc4
Rong Ying <kohrongying@gmail.com>
2019-04-02 20:17:00 +0800

    Edit styling for main form page.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,26 +9,53 @@
         placeholder="read"
         v-model="action"
       />
-      <div class="input-wrapper">
-        <input class="action-item" type="text" v-model="item" />
-        <md-button type="submit" class="md-icon-button">
-          <md-icon>arrow_forward</md-icon>
-        </md-button>
-      </div>
+      <input class="action-item" type="text" v-model="item" />
+
+      <md-button type="submit" class="md-icon-button submit-button">
+        <md-icon>arrow_forward</md-icon>
+      </md-button>
+    
     </form>
   </div>
 </template>
-<style>
+<style scoped>
+.header {
+  font-size: 40px;
+  margin-top: 20px;
+}
+
+input {
+  border: none;
+  font-size: 40px;
+  border: none;
+  border-bottom: 1px #2c3e50 solid;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+
+input:focus {
+  outline: none;
+}
+
 .main-form {
-  padding: 0 180px;
   display: flex;
   align-items: flex-start;
   flex-direction: column;
+  width: 80vw;
+  position: relative;
 }
+
 form {
   text-align: left;
   display: flex;
   flex-direction: column;
+  width: 100%;
+}
+
+.submit-button {
+  position: absolute;
+  right: 0;
+  bottom: 10px;
 }
 </style>
 <script lang="ts">
@@ -73,40 +100,3 @@ export default Vue.use(MdButton)
     }
   });
 </script>
-
-<style scoped>
-.container {
-  display: flex;
-  justify-content: center;
-  padding-top: 100px;
-}
-
-.main {
-  width: 50%;
-  text-align: left;
-}
-
-.header {
-  font-size: 40px;
-  margin-top: 20px;
-}
-
-input {
-  border: none;
-  font-size: 40px;
-  border: none;
-  border-bottom: 1px #2c3e50 solid;
-  color: #2c3e50;
-  margin-top: 20px;
-}
-
-input:focus {
-  outline: none;
-}
-
-.input-wrapper {
-  display: flex;
-  align-items: flex-end;
-}
-
-</style>
